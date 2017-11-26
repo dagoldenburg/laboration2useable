627a7710127cd12d787d2c01b4599a6e023d79af
dagoldenburg <dagoldenburg@gmail.com>
2017-11-26 19:21:45 +0100

    First commit

---
+++ Activities/View/GameView.java
@@ -0,0 +1,29 @@
+package com.example.micke.laboration2.Activities.View;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.example.micke.laboration2.R;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class GameView extends View {
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas){
+        
+    }
+}
